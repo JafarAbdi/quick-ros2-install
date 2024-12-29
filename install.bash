307f94872b72e28b74cd7ec7a66259eaabce99c0
JafarAbdi <jafar.uruc@gmail.com>
2024-12-29 16:24:39 +0000

    Fix tzdata prompt user for input

--- install.bash
+++ install.bash
@@ -47,8 +47,6 @@ fi
 # Installation based on https://docs.ros.org/en/rolling/Installation/Ubuntu-Install-Debs.html
 info "Installing ROS 2 $distro"
 
-export DEBIAN_FRONTEND=noninteractive
-
 locale  # check for UTF-8
 
 sudo apt update && sudo apt install locales
@@ -67,7 +65,8 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-a
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y ros-${distro}-desktop
+# tzdata will prompt for user input, so we need to avoid that
+sudo DEBIAN_FRONTEND=noninteractive sudo apt install -y ros-${distro}-desktop
 sudo apt install -y ros-dev-tools
 
 info "ROS 2 $distro installed successfully"
